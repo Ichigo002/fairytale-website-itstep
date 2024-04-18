dd15b7f93e4fafce5cec626831819469d2adfbae
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-04-18 12:19:49 +0300

    tut

---
+++ webapp/app/Http/Controllers/TodoListController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ListItem;
+
+class TodoListController extends Controller
+{
+    public function saveItem()
+    {
+        return view("welcome");
+    }
+}
--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/saveItem', [TodoListController::class, 'saveItem'])->name('saveItem');
