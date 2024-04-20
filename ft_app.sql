03cb252816919cf21de7cd2e1bfac61db26907f9
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-04-20 20:14:16 +0300

    Update ft_app.sql

--- ft_app.sql
+++ ft_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2024 at 04:56 PM
+-- Generation Time: Apr 20, 2024 at 07:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `ft_app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `biography` text NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `place_of_live` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -69,6 +85,23 @@ CREATE TABLE `failed_jobs` (
   `payload` longtext NOT NULL,
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fairytales`
+--
+
+CREATE TABLE `fairytales` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `id_author` bigint(20) UNSIGNED DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `txt_ukr` longtext NOT NULL,
+  `txt_alt` longtext NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -125,7 +158,11 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
-(3, '0001_01_01_000002_create_jobs_table', 1);
+(3, '0001_01_01_000002_create_jobs_table', 1),
+(4, '2024_04_20_053324_fairytales', 2),
+(5, '2024_04_20_055117_create_authors', 2),
+(6, '2024_04_20_055510_create_uploaded_images', 2),
+(7, '2024_04_20_061516_add_column_to_users_table', 3);
 
 -- --------------------------------------------------------
 
@@ -159,8 +196,24 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('ctS7SQsonqJGZhtjtr0hsidBnK9UCe1oKnDCiewC', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiYmY1WWNzYkJNb1F1a29yR0V3QWkyaURlSXp0Z3RqN3BJSEcxQ3RqbSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fX0=', 1713537253),
-('ExHAFRHt7PWjgLu1H9ovOpi8B9Vx9cr7XGd3kfvZ', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiSDVaRnlsYUJSRkhnMDdpaVNZVFBMalJpTGkxMkRHdkk5RGpCbXA5aSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1713537699);
+('Q2AUwJsBikLwUc9oQMHXpdOCafFyGHkZBJ7571NH', 4, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiTExnajNRYWxtMEdFbUlVanBzQ3NXRGpsaTZDemtvSUVRMk5aRVZRUyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9yZWdpc3RlciI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjQ7fQ==', 1713632041),
+('UYiNo3vZ0WmvTiBXAeYHa0Y0qpLRVD4OcgkFacjN', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiNFNubFc2NFdNOU14ZktjZUJiVlBSd09XeE04Snc0MUVLMFc4Sm1NdCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fX0=', 1713632933),
+('YiLQ5NWXMxLzAc03NtEWUmtnkBKGXyAV5muA31kd', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YToyOntzOjY6Il90b2tlbiI7czo0MDoiUXU2a01uV3hITWgySnJYSEVRb1lybmZWSjhqVlR4MDRkWHVyb2MwYyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1713632041);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploaded_images`
+--
+
+CREATE TABLE `uploaded_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_fairytale` bigint(20) UNSIGNED NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `page_number` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -177,21 +230,28 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `birth_date` date DEFAULT NULL
+  `date_of_birth` date NOT NULL DEFAULT '1900-01-01',
+  `role` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `birth_date`) VALUES
-(1, 'wiktor', 'wiktorbojanowski28@wp.pl', '2024-04-19 11:05:36', '$2y$12$teEtx3sUXok..d3MiFHKnOybUtYjJF3VUpgOTLjSB.QtQoFB1cwxS', NULL, '2024-04-19 11:04:49', '2024-04-19 11:05:36', '2024-04-01'),
-(2, 'hacker', 'dwdwd@wpd.pl', NULL, '$2y$12$DmNDHimkR5CcuHDLHMOp1Oqb1PzG7KUL5ouWLXEBirR5ZelPzu76W', NULL, '2024-04-19 11:06:18', '2024-04-19 11:06:18', '2024-04-10'),
-(3, 'wiktor better', 'genxxvsyy@int.pl', '2024-04-19 11:11:39', '$2y$12$bSShA7Ux4oavH9oe/ik1mO.l0vYVt51PZmhLIHPj1Yb8DTPWR1qvm', NULL, '2024-04-19 11:08:56', '2024-04-19 11:11:39', '2024-04-19');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `date_of_birth`, `role`) VALUES
+(1, 'wiktor', 'wiktorbojanowski28@wp.pl', '2024-04-19 11:05:36', '$2y$12$teEtx3sUXok..d3MiFHKnOybUtYjJF3VUpgOTLjSB.QtQoFB1cwxS', NULL, '2024-04-19 11:04:49', '2024-04-19 11:05:36', '1900-01-01', 1),
+(2, 'hacker', 'dwdwd@wpd.pl', NULL, '$2y$12$DmNDHimkR5CcuHDLHMOp1Oqb1PzG7KUL5ouWLXEBirR5ZelPzu76W', NULL, '2024-04-19 11:06:18', '2024-04-19 11:06:18', '1900-01-01', 1),
+(3, 'wiktor better', 'genxxvsyy@int.pl', '2024-04-19 11:11:39', '$2y$12$bSShA7Ux4oavH9oe/ik1mO.l0vYVt51PZmhLIHPj1Yb8DTPWR1qvm', NULL, '2024-04-19 11:08:56', '2024-04-19 11:11:39', '1900-01-01', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cache`
@@ -211,6 +271,12 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `fairytales`
+--
+ALTER TABLE `fairytales`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `jobs`
@@ -246,6 +312,12 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
+-- Indexes for table `uploaded_images`
+--
+ALTER TABLE `uploaded_images`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -257,9 +329,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fairytales`
+--
+ALTER TABLE `fairytales`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -272,13 +356,19 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `uploaded_images`
+--
+ALTER TABLE `uploaded_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
